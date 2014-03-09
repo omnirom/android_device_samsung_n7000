9e2fa7c9c88d38f8df3b810a91a2e4d36c247ac8
Markus Guidry <markus@gu1dry.com>
2014-03-09 15:27:12 -0400

    n7000: Align omni_n7000.mk with a more standard layout
    Additionally adds a fingerprint (N7000XXLT5) & a README
    
      PS2: Minor comment corrections
    
    Change-Id: I7b109fa045c6a9585a9a033a7222c053572842a3

--- omni_n7000.mk
+++ omni_n7000.mk
@@ -1,3 +1,4 @@
+#
 # Copyright (C) 2013 OmniROM Project
 # Copyright (C) 2012 The CyanogenMod Project
 #
@@ -12,21 +13,30 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+
+# Inherit Omni GSM telephony parts
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit common Omni configurations
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from our omni product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
-
-# Pull in GSM-specific stuff such as APNs
-$(call inherit-product, vendor/omni/config/gsm.mk)
-
+# Inherit device specific configurations
 $(call inherit-product, device/samsung/n7000/device.mk)
 
-# Discard inherited values and use our own instead.
+# Device identifier
+PRODUCT_MODEL := GT-N7000
+PRODUCT_BRAND := samsung
 PRODUCT_NAME := omni_n7000
 PRODUCT_DEVICE := n7000
-PRODUCT_BRAND := samsung
-PRODUCT_MANUFACTURER := Samsung
-PRODUCT_MODEL := GT-N7000
+PRODUCT_MANUFACTURER := samsung
+
+# Set build fingerprint / ID / Prduct Name etc.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=GT-N7000 \
+    TARGET_DEVICE=GT-N7000 \
+    PRIVATE_BUILD_DESC="GT-N7000-user 4.1.2 JZO54K N7000XXLT5 release-keys" \
+    BUILD_FINGERPRINT="samsung/GT-N7000/GT-N7000:4.1.2/JZO54K/N7000XXLT5:user/release-keys"
